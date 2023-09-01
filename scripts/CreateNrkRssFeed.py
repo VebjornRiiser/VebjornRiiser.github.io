426430bc1e112fa8c32265a45e6d556c0e9597a8
VebjornRiiser <12122688+VebjornRiiser@users.noreply.github.com>
2023-09-01 19:31:40 +0200

    Changed to use new api

--- scripts/CreateNrkRssFeed.py
+++ scripts/CreateNrkRssFeed.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
+import asyncio
 from datetime import datetime
 import os
+import time
+import httpx
 import requests
 import json
 PODLISTFILENAME = "PodcastsToUpdate.txt"
+RequestHeaders = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0",
+                  "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                  "Accept-Encoding": "gzip, deflate, br",
+                  "Accept-Language": "en-US,en;q=0.5"}
+
 
 def create_footer(json_object: dict) -> str:
     return """</channel>
@@ -19,12 +27,13 @@ def get_content_length(content_url: str) -> int:
 
 def get_guid(content_url: str) -> str:
     """"Gets the guid from the content url"""
-    return content_url.split("/")[-1].split("_0_")[0]
+    return content_url.split("/")[-1].split("_0_")[0].split(".mp3")[0]
 
 
 def format_date(datestring: str) -> str:
     """Takes the nrk publishedAt date and returns a valid podcast RFC-822 date-time"""
-    return (datetime.fromisoformat(datestring.strip("Z")+".000000").strftime("%a, %d %b %Y %X %Z") + "GMT")
+    return datestring
+    # return (datetime.fromisoformat(datestring.strip("Z")+".000000").strftime("%a, %d %b %Y %X %Z") + "GMT")
 
 
 def sec_to_hour_min_sec(sec: int):
@@ -46,15 +55,22 @@ def replace_illegal_chars(string: str) -> str:
     return string.replace("&", "&amp;").replace("<", " &lt;").replace(">", "&gt;").replace("'", "&apos;").replace('"', "&quot;")
 
 
+def get_podcast_url_name(episode_item):
+    return episode_item.get("_links").get("series").get("name")
+
+
+def get_content_url(episode_item):
+    return f"https://podkast.nrk.no/fil/{get_podcast_url_name(episode_item)}/{get_episode_id(episode_item)}_0_ID192MP3.mp3"
+
+
 class EpisodeItem:
-    def __init__(self, itemjson: dict, content_length=None) -> None:
+    def __init__(self, itemjson: dict, playback_url: list[str], content_length=None) -> None:
         self.title: str = replace_illegal_chars(
             itemjson.get("titles").get("title").strip())
         self.description: str = replace_illegal_chars(
             itemjson.get("titles").get("subtitle").strip())
-        self.datetime_string: str = format_date(itemjson.get("publishedAt"))
-        self.content_url = replace_illegal_chars(
-            itemjson.get("downloadables")[0].get("audio").get("url"))
+        self.datetime_string: str = format_date(itemjson.get("date"))
+        self.content_url = playback_url
         if content_length is not None:
             self.content_length = content_length
         else:
@@ -71,12 +87,15 @@ class EpisodeItem:
         nrk_dict = {'titles': {'title': json["title"]}}
 
 
-def json_to_episodeitem(json_items_object: list[dict]) -> list[EpisodeItem]:
+def json_to_episodeitem(episodes, playback_list) -> list[EpisodeItem]:
     print(f"Converting to EpisodeItem")
     episodeitems: list[EpisodeItem] = []
-    for index, item in enumerate(get_episodes_list(json_items_object)):
-        episodeitems.append(EpisodeItem(item))
-    return episodeitems
+    try:
+        for item, playback_url in zip(episodes, playback_list):
+            episodeitems.append(EpisodeItem(item, playback_url))
+        return episodeitems
+    except Exception as e:
+        print(episodes)
 
 
 def save_ep_items(epitems: list[EpisodeItem], show_title: str):
@@ -88,72 +107,142 @@ def save_ep_items(epitems: list[EpisodeItem], show_title: str):
     return jsonstring
 
 
-def create_header(json_object: dict):
+def create_header(episode: dict):
     return f"""<?xml version="1.0" encoding="UTF-8"?>
     <rss version="2.0"
         xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd"
         xmlns:android="http://schemas.android.com/apk/res/android">
     <channel>
-        <title>CUSTOM: {get_show_title(json_object)}</title>
-        <link>{get_show_link(json_object)}</link>
-        <!--<description>{get_description(json_object)}</description>-->
+        <title>CUSTOM: {get_show_title(episode)}</title>
+        <link>{get_show_link(episode)}</link>
+        <!--<description>{get_description(episode)}</description>-->
         <language>no</language>
         <copyright>NRK © 2022</copyright>
         <category>Comedy</category>
         <image>
-        <title>{get_show_title(json_object)}</title>
-        <url>{get_show_image(json_object)}</url>
-        <link>{get_show_link(json_object)}</link>
+        <title>{get_show_title(episode)}</title>
+        <url>{get_show_image(episode)}</url>
+        <link>{get_show_link(episode)}</link>
         <width>144</width>
         <height>144</height>
         </image>
         """
 
+
 def get_episodes_list(json_object):
     return json_object.get("_embedded").get("episodes")
 
-def get_show_title(json_object):
-    return get_episodes_list(json_object)[0].get("originalTitle").strip().replace("&","&amp;")
 
-def get_show_link(json_object):
-    link = get_episodes_list(json_object)[0].get("_links").get("share").get("href")
+def get_show_title(json_object):
+    return json_object.get("originalTitle").strip().replace("&", "&amp;")
+
+
+def get_episode_link(episode):
+    return episode.get("_links").get("share").get("href")
+
+
+def get_show_link(episode):
+    link = get_episode_link(episode)
     return "/".join(link.split("/")[0:-1])
 
-def get_description(json_object):
-    return "" # need to get separate file https://psapi.nrk.no/playback/metadata/podcast/berrum_beyer_snakker_om_greier/l_911b3bdf-2979-40df-9b3b-df297950dfef
 
-def get_show_image(json_object):
-    get_episodes_list(json_object)[0].get("image")[-1].get("url")
+def get_episode_id(episode_item):
+    return episode_item.get("episodeId")
+
+
+def get_description(json_object):
+    # or maybe not. check under titles
+    return ""  # need to get separate file https://psapi.nrk.no/playback/metadata/podcast/berrum_beyer_snakker_om_greier/l_911b3bdf-2979-40df-9b3b-df297950dfef
+
+
+def get_show_image(episode):
+    return episode.get("squareImage")[-1].get("url")
+
 
 def create_feed(url: str) -> str:
     # response = requests.get(url)
     # if response.ok:
     full_rss_feed = ""
-    json_object = None
-    with open("./scripts/episodes.json", 'r') as episode_json_file:
-        json_object = json.loads(episode_json_file.read())
+    # json_object = None
+    # with open("./scripts/episodes.json", 'r', encoding="utf-8") as episode_json_file:
+    #     json_object = json.loads(episode_json_file.read())
+
+    episode_json_list = get_all_episode_items(url)
+    # Need to get all the episodes.json until there is no more and then get all the playback pages
     # json_object = json.loads(response.text)
-    full_rss_feed += create_header(json_object)
-    full_rss_feed += create_episode_items(json_object)
-    full_rss_feed += create_footer(json_object)
+    full_rss_feed += create_header(episode_json_list[0])
+    full_rss_feed += create_episode_items(episode_json_list)
+    full_rss_feed += create_footer(episode_json_list[0])
     # else:
     #     print(f"Bad response from {url}. Got {response.status_code}")
     return full_rss_feed
 
+
 def get_json_from_url(url: str):
     pass
 
-def create_episode_items(json_object: dict) -> str:
-    number_of_episodes = len(get_episodes_list(json_object))
+
+def get_all_episode_items(base_url: str, requests_per_second=2):
+    more_episodes_to_get = True
+    page_index = 1
+    episode_json_list: list[str] = []
+    while more_episodes_to_get:
+        url = base_url+str(page_index)
+
+        response = requests.get(url, headers=RequestHeaders)
+
+        response.raise_for_status()
+
+        episode_json_list.extend(get_episodes_list(json.loads(response.text)))
+
+        if get_number_of_episodes_from_episode_json(response.text) != 50:
+            break
+        page_index += 1
+        time.sleep(1/requests_per_second)
+
+    return episode_json_list
+
+
+def get_number_of_episodes_from_episode_json(json_str: str) -> int:
+    object: dict = json.loads(json_str)
+    eps = object.get("_embedded").get("episodes")
+    return len(eps)
+
+
+def get_all_playback_urls(episode_list: list[str]) -> list[str]:
+
+    urls_to_fetch = [
+        f"https://psapi.nrk.no/playback/manifest/podcast/{get_episode_id(episode)}" for episode in episode_list]
+    loop = asyncio.get_event_loop()
+    EpisodeResults: list[httpx.Response] = loop.run_until_complete(
+        fetch_urls(urls_to_fetch))
+
+    playback_urls: list[str] = []
+    for response in EpisodeResults:
+        if response.status_code != 200:
+            raise Exception("Got status code 200.")
+
+        playback_urls.append(get_playback_url(response.text))
+    return playback_urls
+
+
+def get_playback_url(json_str):
+    return json.loads(json_str).get("playable").get("assets")[0].get("url")
+
+
+def create_episode_items(episodes: dict) -> str:
+    playback_urls = get_all_playback_urls(episodes)
+
+    items = json_to_episodeitem(episodes, playback_urls)
+
+    return generate_episode_rss(items)
+
+
+def generate_episode_rss(items):
     itemstring = ""
-    # Get all items until number of episodes == 0
-    # combine them into a master episode item and then get all of them (could just replace _embedded.episodes)
-    items = json_to_episodeitem(json_object.get("items"))
-    for index, EpItem in enumerate(items):
-        if index % 5 == 0:
-            print(f"\rProgress {round(((index+1)/number_of_episodes)*100, 0)}%")
-            
-        itemstring += f"""<item>
+    for EpItem in items:
+        itemstring += f"""
+        <item>
             <title>{EpItem.title}</title>
             <description>{EpItem.description}</description>
             <pubDate>{EpItem.datetime_string} </pubDate>
@@ -164,28 +253,48 @@ def create_episode_items(json_object: dict) -> str:
         """
     return itemstring
 
+
+async def fetch_urls(urls, requests_per_second=25):
+    async with httpx.AsyncClient(headers=RequestHeaders) as client:
+        sem = asyncio.Semaphore(requests_per_second)
+        tasks = [fetch_url(client, url, sem) for url in urls]
+        results = await asyncio.gather(*tasks)
+        return results
+
+
+async def fetch_url(client: httpx.Client, url, semaphore: asyncio.Semaphore):
+    async with semaphore:
+
+        response: httpx.Response = await client.get(url)
+        response.raise_for_status()
+
+        await asyncio.sleep(1)
+
+    return response
+
+
 if __name__ == "__main__":
-    podnames = ["berrum_beyer_snakker_om_greier",
-                "abels_taarn",
-                "hele_historien",
-                "trygdekontoret",
-                "loerdagsraadet",
-                "debatten",
-                "radio_moerch"]
-    #example url https://nrk-pod-pd.telenorcdn.net/podkast/podcastpublisher_prod/berrum_beyer_snakker_om_greier/9aff0403-327c-4e0b-bf04-03327cce0b37.mp3
+    podnames = [
+        "berrum_beyer_snakker_om_greier",
+        "trygdekontoret",
+        "abels_taarn",
+        "hele_historien",
+        "loerdagsraadet",
+        "debatten",
+        "radio_moerch"]
+    # example url https://nrk-pod-pd.telenorcdn.net/podkast/podcastpublisher_prod/berrum_beyer_snakker_om_greier/9aff0403-327c-4e0b-bf04-03327cce0b37.mp3
     if (os.path.exists(PODLISTFILENAME)):
         print(f"Found {PODLISTFILENAME}. Using to lookup podcasts")
         with open(PODLISTFILENAME, 'r') as PodcastListFile:
             podnames = PodcastListFile.readlines()
 
-
     for pod in podnames:
-        page_index = 1
-        # for page_index in range(1,2):
-            # må hente ut alle før man skriver
-        url = f"https://psapi.nrk.no/radio/catalog/podcast/{pod}/episodes?page={page_index}&pageSize=50&sort=desc"
-        rss_feed = create_feed(url)
+        print(f"Getting rss feed for '{pod}'")
+        baseurl = f"https://psapi.nrk.no/radio/catalog/podcast/{pod}/episodes?pageSize=50&sort=desc&page="
+        rss_feed = create_feed(baseurl)
         if rss_feed == "":
             continue
         with open(f"{pod}.rss", 'w', encoding="utf-8") as feedfile:
             feedfile.write(rss_feed)
+        print(f"Done with '{pod}' sleeping before starting next")
+        time.sleep(2)
